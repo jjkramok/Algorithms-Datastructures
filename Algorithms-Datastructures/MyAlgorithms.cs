5ea4ef0d43c2e0973e7571b4c4f1a51801ea0a77
Tim Hintzbergen <t.a.h.hintzbergen@student.utwente.nl>
2017-09-06 15:27:24 +0200

    sieve of Eratostheres

---
+++ Algorithms-Datastructures/MyAlgorithms.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms_Datastructures
+{
+    internal class MyAlgorithms
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        public List<int> sieveOfEratosthenes(int p)
+        {
+            SortedDictionary<int, bool> N = new SortedDictionary<int, bool>();
+            for (var i = 2; i < p; i++)
+            {
+                N.Add(i, true);
+            }
+            foreach (KeyValuePair<int, bool> entry in N)
+            {
+                Console.WriteLine(entry.Key + ", ");
+            }
+            foreach (KeyValuePair<int, bool> entry in N)
+            {
+                if (entry.Value)
+                {
+                    int i = entry.Key;
+                    while (i < p)
+                    {
+                        i += entry.Key;
+                    }
+                    
+                }
+                    
+            }
+            
+            
+            return null;
+        }
+    }
+}
