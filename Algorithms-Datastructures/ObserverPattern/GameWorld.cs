1528d32300a48d696966a4945f5bb3bb26e8b7d9
Kramok Desktop <jjkramok@gmail.com>
2017-09-13 16:28:23 +0200

    working on the Observer pattern, making a simple game with observers awaiting a new gametick

---
+++ Algorithms-Datastructures/ObserverPattern/GameWorld.cs
@@ -0,0 +1,43 @@
+﻿using System.CodeDom;
+using System.Diagnostics;
+
+namespace ObserverPattern
+{
+    public class GameWorld : Observable
+    {
+        private Stopwatch _gametime;
+        private GameWorld instance;
+        public bool shutdown { get; set; } = false;
+        private const long TimePerTick = 1000;
+
+        private GameWorld()
+        {
+            _gametime = new Stopwatch();
+        }
+
+        public void StartGame()
+        {
+            // Gives of a signal that updates all game components
+            _gametime.Start();
+            while (!shutdown)
+            {
+                if (_gametime.ElapsedMilliseconds >= TimePerTick)
+                {
+                    _gametime.Restart();
+                    notify();
+                }
+            }
+            _gametime.Reset();
+        }
+
+        public GameWorld getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new GameWorld();
+            }
+            return instance;
+        }
+        
+    }
+}
---
+++ Algorithms-Datastructures/ObserverPattern/Observable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace ObserverPattern
+{
+    public abstract class Observable
+    {
+        private HashSet<IObserver> observers = new HashSet<IObserver>();
+
+        public bool attach(IObserver obs)
+        {
+            return observers.Add(obs);
+        }
+
+        public bool detach(IObserver obs)
+        {
+            return observers.Remove(obs);
+        }
+
+        public void notify()
+        {
+            foreach (IObserver observer in observers)
+            {
+                observer.Update();
+            }
+        }
+    }
+}
