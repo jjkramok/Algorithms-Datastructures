00396063c8c0822e43adb9bdc9fe0e2501244fa9
Tim Hintzbergen <t.a.h.hintzbergen@student.utwente.nl>
2017-09-20 15:58:05 +0200

    working on stack for undoing previous commands

--- Algorithms-Datastructures/CommandPattern/RemoteControl.cs
+++ Algorithms-Datastructures/CommandPattern/RemoteControl.cs
@@ -1,17 +1,26 @@
-﻿namespace CommandPattern
+﻿using System.Collections.Generic;
+
+namespace CommandPattern
 {
     public class RemoteControl
     {
         public ICommand Command { get; set; }
+        public Stack<ICommand> PrevCommands = new Stack<ICommand>();
 
         public void PressExecute()
         {
             Command.Execute();
+            PrevCommands.Push(Command);
         }
 
         public void PressUndo()
         {
-            Command.Undo();
+            if (PrevCommands.Count > 0)
+            {
+                Command.Undo();
+                PrevCommands.Pop(); 
+            }
+            //TODO work with PrevCommands stack for undoing stuff
         }
     }
 }
