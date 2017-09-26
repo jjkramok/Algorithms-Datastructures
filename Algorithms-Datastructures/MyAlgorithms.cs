9e7fb2803a5130fdb1183ea7eeeee2f366dbc326
Tim Hintzbergen <t.a.h.hintzbergen@student.utwente.nl>
2017-09-26 14:12:56 +0200

    working on own quicksort

--- Algorithms-Datastructures/MyAlgorithms.cs
+++ Algorithms-Datastructures/MyAlgorithms.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
 
 namespace Algorithms_Datastructures
 {
-    internal class MyAlgorithms
+    internal class MyAlgorithms<T> where T : IComparable<T>
     {
         public static void Main(string[] args)
         {
@@ -58,6 +62,72 @@ namespace Algorithms_Datastructures
         {
             //todo base case
             return n * FacRec(n - 1);
+        }
+        
+        public static List<T> QuickSort(List<T> l) {
+            if (l.Count <= 1)
+            {
+                return l;
+            }
+
+            var pivotIndex = FindPivotIndex(l);
+            T pivot = l[pivotIndex];
+            
+            //Move pivot to last position
+            Swap(pivotIndex, l.Count-1, l);
+
+            // TODO ensure list.Count > 1
+            // TODO do insertion sort on smaller lists
+            // TODO list with 3 elements or more (see above) should be sorted with insertion sort and returned (avoid doing mot on a three element list)
+            // Loop from both direction (ignore pivot!) and swap variables into their respective parts of the list
+            int i = 0;
+            int j = l.Count - 2;
+            while (i > j)
+            {
+                if (l[i].CompareTo(pivot) > 0 && l[j].CompareTo(pivot) < 0)
+                {
+                    Swap(i, j, l);
+                }
+                else
+                {
+                    i++;
+                }
+
+                if (l[j].CompareTo(pivot) < 0 && l[i].CompareTo(pivot) > 0)
+                {
+                    Swap(i, j, l);
+                }
+                else
+                {
+                    j++;
+                }
+            }
+            
+            
+            // TODO first replace pivot with something in the greater sublist
+            // Swap pivot back into the 'greater than pivot' part of the list
+            Swap(i, l.Count-1, l);
+            
+            // Continue QuickSorting on both sublists and then stitch the results together
+            List<T> lower = QuickSort(l.GetRange(0, i+1));
+            List<T> greater = QuickSort(l.GetRange(i+1, l.Count - i)); //TODO does this work? check bounds
+            lower.AddRange(greater);
+            return lower;
+        }
+
+        private static int FindPivotIndex(List<T> list)
+        {
+            //TODO implement Median Of Three to find pivot
+            //T[] mot = {list[0], list[list.Count / 2], list[list.Count - 1]}; 
+            Random rnd = new Random();
+            return rnd.Next(0, list.Count);
+        }
+
+        private static void Swap(int i, int j, List<T> l)
+        {
+            T temp = l[i];
+            l[i] = l[j];
+            l[j] = temp;
         }
     }
 }
