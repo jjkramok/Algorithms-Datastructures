b52c4069cebd38b21e81721287c570f4a140362d
Kramok Desktop <jjkramok@gmail.com>
2017-10-10 13:09:06 +0200

    working on heap

---
+++ Algorithms-Datastructures/Trees/Heap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Algorithms_Datastructures.Trees
+{
+    public class Heap<T> where T : IComparable<T>
+    {
+        private T[] _heap = new T[10];
+        public int Size = 0;
+        
+        public void Insert(T item)
+        {
+            if (_heap.Length == Size)
+            {
+                
+            }
+        }
+
+    }
+}
