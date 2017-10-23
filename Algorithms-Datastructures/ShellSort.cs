e7e5fe9dbdfbcbe8e5d536bba429a4009c32fa97
Tim Hintzbergen <t.a.h.hintzbergen@student.utwente.nl>
2017-10-23 14:01:42 +0200

    finished shellsort

--- Algorithms-Datastructures/ShellSort.cs
+++ Algorithms-Datastructures/ShellSort.cs
@@ -7,14 +7,33 @@ namespace Algorithms_Datastructures
     {
         public ShellSort()
         {
-            
+            Random rng = new Random();
+            List<int> array = new List<int>();
+            for (int i = 0; i < 20; i++)
+            {
+                array.Add(rng.Next(1, 100));
+            }
+            Console.WriteLine("Random input array:\n[{0}]", string.Join(", ", array));
+            Sort(array);
+            Console.WriteLine("Random input array:\n[{0}]", string.Join(", ", array));
         }
 
-        public static List<T> Sort<T>(List<T> array) where T : IComparable<T>
+        public static void Sort<T>(List<T> array) where T : IComparable<T>
         {
-            return null;
+            // Keep shellsorting for each gap, use magic number 2.2 for new gap calculation (best practise)
+            for (int gap = (int) Math.Round(array.Count / 2.2); gap > 0; gap = gap == 2 ? 1 : (int) (gap / 2.2))
+            {
+                // Start at gap so we can compare backwards
+                for (int i = gap; i < array.Count; i++)
+                {
+                    T temp = array[i];
+                    int j = i;
+                    // Do 'InsertionSort' on all elements in this gap sequence
+                    for (; j >= gap && temp.CompareTo(array[j - gap]) < 0; j -= gap)
+                        array[j] = array[j - gap];
+                    array[j] = temp;
+                }   
+            }
         }
-        
-        
     }
 }
